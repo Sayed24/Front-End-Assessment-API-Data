fb934ae8cd065333e9fdf25abc931a989f663e99
Sayed Sadat <SadatSayed92@gmail.com>
2022-06-27 12:55:25 -0700

    updates

--- src/mycomponent.js
+++ src/mycomponent.js
@@ -9,6 +9,7 @@ const MyComponent = () => {
     const [data, setData] = useState([])
 
 
+    // Fethcing data from the API
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
@@ -28,6 +29,8 @@ const MyComponent = () => {
     // const studentData = data && data.students;
     // console.log("Student data: " + studentData)
 
+
+    // Search filter by name
     const handleSearch = (event) => {
         let value = event.target.value.toLowerCase();
         let result = [];
@@ -38,9 +41,23 @@ const MyComponent = () => {
     const [filteredData, setFilteredData] = useState(data);
     console.log(filteredData);
 
+
+    // // Search by Tag name
+    // const handleSearch = (event) => {
+    //     let value = event.target.value.toLowerCase();
+    //     let result = [];
+    //     console.log(value);
+    //     result = data.filter((data) => { return data.FirstName.search(value) !== -1; });
+    //     setFilteredData(result);
+    // }
+    // const [filteredData, setFilteredData] = useState(data);
+    // console.log(filteredData);
+
+    // Grade average
     let sum = data.reduce((a, b) => a + b, 0);	//get sum of all elements in array
     let average = (sum / data.length) || 0; //get average of all elements in array ;)
 
+    // Adding Tags
     const [datas, setDatas] = useState([]);
     const [dataa, setDataa] = useState('');
 
@@ -70,6 +87,8 @@ const MyComponent = () => {
             {!loading && (
                 <div>
                     <h2 className='title'>Frontend Assessment - Student Profiles</h2>
+
+                    {/* rendering student filter search by name */}
                     <div>
                         <input className='student-search' type="text" onChange={handleSearch} placeholder="Search by name" />
                         {/* <p>{filteredData}</p> */}
@@ -77,10 +96,14 @@ const MyComponent = () => {
                             <p>{student}</p>
                         ))} */}
                     </div>
+
+                    {/* rendering student search by tag name */}
                     <div>
                         <input className='student-search' type="text" onChange={handleSearch} placeholder="Search by tag" />
                         {/* <p>{filteredData}</p> */}
                     </div>
+
+                    {/* redering data from API  */}
                     <ul className='student-list'>
                         {data?.map((student, index) => (
                             <div className="student-items" key={`student-${index + 1}`}>
@@ -89,7 +112,7 @@ const MyComponent = () => {
                                         <img className='student-pic' src={student.pic} alt={`Pic for Student ${index + 1}`} />
                                     </div>
                                     <div className='student-data'>
-                                        <li><h1>{student.firstName}&nbsp;{student.lastName}</h1></li> <span></span>
+                                        <li><h1>{student.firstName}&nbsp;{student.lastName}</h1></li>
                                         <div className='student-info'>
                                             <li><strong>Email: </strong> {student.email}</li>
                                             <li><strong>Company: </strong> {student.company}</li>
@@ -97,9 +120,10 @@ const MyComponent = () => {
                                             <li><strong>Average: </strong> {average}</li>
 
                                             {datas.map(dataa => (
-                                                <span className='student-tag-item'>{dataa}</span>
+                                                <span className='student-tag-item'> {dataa} </span>
                                             ))}
 
+                                            {/* rendering collapse data and set them to hide */}
                                             <div class="collapse hide" id="myCollapse">
                                                 <ol>
                                                     {student.grades.map((grade, index) => (
@@ -110,13 +134,18 @@ const MyComponent = () => {
                                                     ))}
                                                 </ol>
                                             </div>
+
+                                            {/* adding tag input */}
                                             <div className='student-grades'>
                                                 <input type="text" onChange={handleChange} onClick={addTagHandler} placeholder="Add a tag" className='student-tag' />
                                                 {/* <input type="text" value={value} onChange={handleChange} onKeyPress={handleKeypress} onClick={handleSubmit} placeholder="Add a tag" className='student-tag' /> */}
                                             </div>
+
                                         </div>
                                     </div>
                                 </div>
+
+                                {/* + icon for collapse button */}
                                 <div className='button-col'>
                                     <button type="button" class="btn btn-secondary ms-5 my-5 " data-bs-toggle="collapse" data-bs-target="#myCollapse">+</button>
                                 </div>
